0de70a471e52c8c0fc22227e31d09c1b162921f0
Mike Lee Williams <mike@mike.place>
2020-11-26 12:55:42 -0800

    Reformat with vim-prettier

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './tailwind.css';
 
 class Tasklist extends React.Component {
   handleChange(_, taskId) {
-    this.props.toggleTaskDone(taskId)
+    this.props.toggleTaskDone(taskId);
   }
 
   render() {
-    const items = (
-      this.props.tasks.map((task) =>
-        <li key={task.id}><label>
+    const items = this.props.tasks.map((task) => (
+      <li key={task.id}>
+        <label>
           <input
             defaultChecked={task.done}
             // onChange is passed a single argument, the event, by the browser. We
@@ -19,45 +19,43 @@ class Tasklist extends React.Component {
             type="checkbox"
             className="mr-2"
           />
-          <span className={task.done ? "line-through text-gray-400" : ""}>{task.text}</span>
-        </label></li>
-      )
-    )
-    return (
-      <ul className="list-none my-2">
-        {items}
-      </ul>
-    )
+          <span className={task.done ? 'line-through text-gray-400' : ''}>
+            {task.text}
+          </span>
+        </label>
+      </li>
+    ));
+    return <ul className="list-none my-2">{items}</ul>;
   }
 }
 
 class Taskentry extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      taskText: ''
-    }
+      taskText: '',
+    };
   }
 
   handleChange = (event) => {
     this.setState({
-      taskText: event.target.value
-    })
-  }
+      taskText: event.target.value,
+    });
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (this.state.taskText === '') {
-      return
+      return;
     }
-    this.props.addTask(this.state.taskText)
+    this.props.addTask(this.state.taskText);
     this.setState({
-      taskText: ''
-    })
-  }
+      taskText: '',
+    });
+  };
 
   render() {
-    const isTask = this.state.taskText !== ''
+    const isTask = this.state.taskText !== '';
     return (
       <form onSubmit={this.handleSubmit}>
         <input
@@ -68,12 +66,12 @@ class Taskentry extends React.Component {
           onChange={this.handleChange}
         />
         <input
-          className={"border mx-2 px-2 " + (isTask ? "" : "text-gray-400")}
+          className={'border mx-2 px-2 ' + (isTask ? '' : 'text-gray-400')}
           type="submit"
           value="Add task"
         />
       </form>
-    )
+    );
   }
 }
 
@@ -82,36 +80,37 @@ class App extends React.Component {
     super(props);
     this.state = {
       tasks: [
-        {id: 0, done: true, text: 'Buy milk'},
-        {id: 1, done: false, text: 'Call mum'}
-      ]
-    }
+        { id: 0, done: true, text: 'Buy milk' },
+        { id: 1, done: false, text: 'Call mum' },
+      ],
+    };
   }
 
   toggleTaskDone(taskId) {
-    const tasks = this.state.tasks
-    const idx = this.state.tasks.findIndex((task) => task.id === taskId)
-    tasks[idx].done = (tasks[idx].done ? false : true)
+    const tasks = this.state.tasks;
+    const idx = this.state.tasks.findIndex((task) => task.id === taskId);
+    tasks[idx].done = tasks[idx].done ? false : true;
     this.setState({
-      tasks
-    })
+      tasks,
+    });
   }
 
   addTask(taskText) {
     const tasks = this.state.tasks;
-    const maxId = this.state.tasks.length ?
-      Math.max(...this.state.tasks.map((task) => task.id)) : 0
+    const maxId = this.state.tasks.length
+      ? Math.max(...this.state.tasks.map((task) => task.id))
+      : 0;
     this.setState({
       // put new task at start of list
-      tasks: [{done:false, text: taskText, id: maxId + 1}].concat(tasks)
-    })
+      tasks: [{ done: false, text: taskText, id: maxId + 1 }].concat(tasks),
+    });
   }
 
   clearDone(event) {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
-      tasks: this.state.tasks.filter((task) => !task.done)
-    })
+      tasks: this.state.tasks.filter((task) => !task.done),
+    });
   }
 
   render() {
@@ -119,7 +118,7 @@ class App extends React.Component {
       <form onSubmit={(event) => this.clearDone(event)}>
         <input className="border px-2" type="submit" value="Clear done" />
       </form>
-    )
+    );
     return (
       <div className="px-5 py-5">
         <h1 className="font-bold text-lg">Todo</h1>
@@ -130,7 +129,7 @@ class App extends React.Component {
         />
         {clearDoneButton}
       </div>
-    )
+    );
   }
 }
 
