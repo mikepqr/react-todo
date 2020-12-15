602c7b9c0bdd3f7bc85db8f71d9feb2fa734ccf1
Mike Lee Williams <mike@mike.place>
2020-12-14 23:35:25 -0800

    Run prettier

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const DEFAULT_TASKS = [
   { id: 1, done: false, text: "Call mum" },
 ];
 
-const NewTaskEntry = props => {
+const NewTaskEntry = (props) => {
   const [taskText, setTaskText] = useState("");
 
   const handleSubmit = (event) => {
@@ -37,17 +37,17 @@ const NewTaskEntry = props => {
       />
     </form>
   );
-}
+};
 
-const ClearDoneButton = props => {
+const ClearDoneButton = (props) => {
   return (
     <form onSubmit={props.clearDone}>
       <input className="border px-2" type="submit" value="Clear done" />
     </form>
   );
-}
+};
 
-const TaskListItem = props => {
+const TaskListItem = (props) => {
   return (
     <li>
       <label>
@@ -63,11 +63,12 @@ const TaskListItem = props => {
       </label>
     </li>
   );
-}
+};
 
 function TaskList(props) {
   const items = props.tasks.map((task) => (
-    <TaskListItem key={task.id}
+    <TaskListItem
+      key={task.id}
       task={task}
       onChangeTaskIdDone={() => props.onChangeTaskDone(task.id)}
     />
@@ -78,40 +79,35 @@ function TaskList(props) {
 const Project = (props) => {
   // Deep copy DEFAULT_TASKS, otherwise Projects share state. Round trip to
   // JSON is a simple way to do deep copy.
-  const [tasks, setTasks] = useState(JSON.parse(JSON.stringify(DEFAULT_TASKS)))
+  const [tasks, setTasks] = useState(JSON.parse(JSON.stringify(DEFAULT_TASKS)));
 
   const handleChangeTaskDone = (taskId) => {
     const idx = tasks.findIndex((task) => task.id === taskId);
     const _tasks = tasks.slice();
     _tasks[idx].done = _tasks[idx].done ? false : true;
-    setTasks(_tasks)
+    setTasks(_tasks);
   };
 
   const addTask = (taskText) => {
-    const maxId = tasks.length
-      ? Math.max(...tasks.map((task) => task.id))
-      : 0;
+    const maxId = tasks.length ? Math.max(...tasks.map((task) => task.id)) : 0;
     // put new task at start of list
-    setTasks([{ done: false, text: taskText, id: maxId + 1 }].concat(tasks))
+    setTasks([{ done: false, text: taskText, id: maxId + 1 }].concat(tasks));
   };
 
   const clearDone = (event) => {
     event.preventDefault();
-    setTasks(tasks.filter(task => !task.done))
+    setTasks(tasks.filter((task) => !task.done));
   };
 
   return (
     <div className="px-5 py-5">
       <h1 className="font-bold text-lg">{props.projectName}</h1>
       <NewTaskEntry addTask={addTask} />
-      <TaskList
-        tasks={tasks}
-        onChangeTaskDone={handleChangeTaskDone}
-      />
+      <TaskList tasks={tasks} onChangeTaskDone={handleChangeTaskDone} />
       <ClearDoneButton clearDone={clearDone} />
     </div>
   );
-}
+};
 
 function App() {
   return (
