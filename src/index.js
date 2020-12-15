905a26d4b4734de81eba3cd9287d7463d799232b
Mike Lee Williams <mike@mike.place>
2020-12-14 22:12:54 -0800

    Convert Project to functional/useState

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const DEFAULT_TASKS = [
   { id: 1, done: false, text: "Call mum" },
 ];
 
-const NewTaskEntry = (props) => {
+const NewTaskEntry = props => {
   const [taskText, setTaskText] = useState("");
 
   const handleSubmit = (event) => {
@@ -39,7 +39,7 @@ const NewTaskEntry = (props) => {
   );
 }
 
-function ClearDoneButton(props) {
+const ClearDoneButton = props => {
   return (
     <form onSubmit={props.clearDone}>
       <input className="border px-2" type="submit" value="Clear done" />
@@ -47,7 +47,7 @@ function ClearDoneButton(props) {
   );
 }
 
-function TaskListItem(props) {
+const TaskListItem = props => {
   return (
     <li>
       <label>
@@ -75,64 +75,49 @@ function TaskList(props) {
   return <ul className="list-none my-2">{items}</ul>;
 }
 
-class Project extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // Deep copy DEFAULT_TASKS, otherwise Projects share state. Round trip to
-      // JSON is a simple way to do deep copy.
-      tasks: JSON.parse(JSON.stringify(DEFAULT_TASKS)),
-      projectName: "Project",
-    };
-  }
+const Project = (props) => {
+  // Deep copy DEFAULT_TASKS, otherwise Projects share state. Round trip to
+  // JSON is a simple way to do deep copy.
+  const [tasks, setTasks] = useState(JSON.parse(JSON.stringify(DEFAULT_TASKS)))
 
-  handleChangeTaskDone = (taskId) => {
-    const tasks = this.state.tasks;
-    const idx = this.state.tasks.findIndex((task) => task.id === taskId);
-    tasks[idx].done = tasks[idx].done ? false : true;
-    this.setState({
-      tasks,
-    });
+  const handleChangeTaskDone = (taskId) => {
+    const idx = tasks.findIndex((task) => task.id === taskId);
+    const _tasks = tasks.slice();
+    _tasks[idx].done = _tasks[idx].done ? false : true;
+    setTasks(_tasks)
   };
 
-  addTask = (taskText) => {
-    const tasks = this.state.tasks;
-    const maxId = this.state.tasks.length
-      ? Math.max(...this.state.tasks.map((task) => task.id))
+  const addTask = (taskText) => {
+    const maxId = tasks.length
+      ? Math.max(...tasks.map((task) => task.id))
       : 0;
-    this.setState({
-      // put new task at start of list
-      tasks: [{ done: false, text: taskText, id: maxId + 1 }].concat(tasks),
-    });
+    // put new task at start of list
+    setTasks([{ done: false, text: taskText, id: maxId + 1 }].concat(tasks))
   };
 
-  clearDone = (event) => {
+  const clearDone = (event) => {
     event.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.filter((task) => !task.done),
-    });
+    setTasks(tasks.filter(task => !task.done))
   };
 
-  render() {
-    return (
-      <div className="px-5 py-5">
-        <h1 className="font-bold text-lg">{this.state.projectName}</h1>
-        <NewTaskEntry addTask={this.addTask} />
-        <TaskList
-          tasks={this.state.tasks}
-          onChangeTaskDone={this.handleChangeTaskDone}
-        />
-        <ClearDoneButton clearDone={this.clearDone} />
-      </div>
-    );
-  }
+  return (
+    <div className="px-5 py-5">
+      <h1 className="font-bold text-lg">{props.projectName}</h1>
+      <NewTaskEntry addTask={addTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTaskDone={handleChangeTaskDone}
+      />
+      <ClearDoneButton clearDone={clearDone} />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      <Project />
-      <Project />
+      <Project projectName="A" />
+      <Project projectName="B" />
     </div>
   );
 }
